44e03574b1a07d5434e0d20c3212f93c68b30033
TomaszKot11 <kottomasz98@gmail.com>
2019-08-06 00:56:31 +0200

    Backup of work

---
+++ HearthstoneCardView/CardTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CardTableViewCell.swift
+//  HearthstoneCardView
+//
+//  Created by Tomasz Kot on 06/08/2019.
+//  Copyright © 2019 Tomasz Kot. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CardTableViewCell : UITableViewCell {
+    
+    @IBOutlet weak var manaImageView: UIImageView!
+    @IBOutlet weak var nameTextView: UITextField!
+    @IBOutlet weak var classTextView: UITextField!
+    @IBOutlet weak var rarityTextView: UITextField!
+}
--- HearthstoneCardView/CardsIndexTableViewController.swift
+++ HearthstoneCardView/CardsIndexTableViewController.swift
@@ -13,5 +13,19 @@ import CoreData
 
 class CardsIndexTableViewController: UITableViewController {
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
     
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CardTableViewCell", for: indexPath)
+        
+
+        return cell
+    }
 }
